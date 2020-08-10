4285f9ac292d6f2e95d3cce8aa4cf6905a8df893
Chun Liu <chunliu@outlook.com>
2020-08-10 17:28:11 +0800

    almost done

--- roles/drupal/files/installdrupal.sh
+++ roles/drupal/files/installdrupal.sh
@@ -6,31 +6,32 @@ ACCOUNT_PASS=${3}
 DB_URL=${4}
 
 DRUPAL_ROOT=/var/www/drupal/web
+SHARED_ROOT=/gfstorage/drupalsite
 
-if [ ! -d /mnt/drupalsite/files ] && [ ! -f /mnt/drupalsite/drupal.lock ]; then
-    touch /mnt/drupalsite/drupal.lock
-    echo "first node: " >> /mnt/drupalsite/drupal.lock
-    echo $(hostname) >> /mnt/drupalsite/drupal.lock
+if [ ! -d $SHARED_ROOT/files ] && [ ! -f $SHARED_ROOT/drupal.lock ]; then
+    touch $SHARED_ROOT/drupal.lock
+    echo "first node: " >> $SHARED_ROOT/drupal.lock
+    echo $(hostname) >> $SHARED_ROOT/drupal.lock
     IS_FIRST_NODE=true
     echo "lock created!"
 fi
 
 if [ "$IS_FIRST_NODE" = true ]; then
-    mkdir -p /mnt/drupalsite/files
+    mkdir -p $SHARED_ROOT/files
 fi
 
 cd $DRUPAL_ROOT/sites/default/
 if [ ! -d files ]; then
-    ln -s /mnt/drupalsite/files files
+    ln -s $SHARED_ROOT/files files
 fi
 
 if [ "$IS_FIRST_NODE" = true ]; then
-    cp default.settings.php /mnt/drupalsite/settings.php
-    cp default.services.yml /mnt/drupalsite/services.yml
+    cp default.settings.php $SHARED_ROOT/settings.php
+    cp default.services.yml $SHARED_ROOT/services.yml
     echo "copied settings.php and services.yml to file share"
-    echo "copied settings.php and services.yml to file share" >> /mnt/drupalsite/drupal.lock
+    echo "copied settings.php and services.yml to file share" >> $SHARED_ROOT/drupal.lock
 else
-    while [ ! -d /mnt/drupalsite/files/js ];
+    while [ ! -f $SHARED_ROOT/firstnode.done ];
     do
         sleep 30
         echo "Sleeping, wait for 1st node to install the drupal site."
@@ -38,8 +39,8 @@ else
     echo "directory created. exit sleep loop."
 fi
 
-ln -s /mnt/drupalsite/settings.php ./settings.php
-ln -s /mnt/drupalsite/services.yml ./services.yml
+ln -s $SHARED_ROOT/settings.php ./settings.php
+ln -s $SHARED_ROOT/services.yml ./services.yml
 echo "created sym links."
 
 if [ "$IS_FIRST_NODE" = true ]; then
@@ -49,12 +50,13 @@ if [ "$IS_FIRST_NODE" = true ]; then
     chmod 777 $DRUPAL_ROOT/sites/default/settings.php
     chmod 777 $DRUPAL_ROOT/sites/default/services.yml
     echo "modified permission on files."
-    echo "modified permission on files." >> /mnt/drupalsite/drupal.lock
+    echo "modified permission on files." >> $SHARED_ROOT/drupal.lock
     # install drupal site
     cd $DRUPAL_ROOT
     echo "start to install drupal site."
-    echo "start to install drupal site." >> /mnt/drupalsite/drupal.lock
-    ../vendor/drush/drush/drush si -y --site-name=$SITE_NAME --account-name=$ACCOUNT_NAME --account-pass=$ACCOUNT_PASS --db-url=$DB_URL
+    echo "start to install drupal site." >> $SHARED_ROOT/drupal.lock
+    ../vendor/drush/drush/drush si standard -y --site-name="$SITE_NAME" --account-name="$ACCOUNT_NAME" --account-pass="$ACCOUNT_PASS" --db-url="$DB_URL"
     echo "drupal site created."
-    echo "drupal site created." >> /mnt/drupalsite/drupal.lock
+    echo "drupal site created." >> $SHARED_ROOT/drupal.lock
+    touch $SHARED_ROOT/firstnode.done
 fi
